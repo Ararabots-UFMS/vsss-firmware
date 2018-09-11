e817d00cafad8c77017c44098bb88b98bfa9f9d7
Leonardo Esperanca <leonardo.oliveira.esperanca@gmail.com>
2018-09-11 16:46:21 -0400

    calculos PID corrigidos, DMP MPU6050 funcionando, firmware documentado

--- src/principal_esp/Defines.h
+++ src/principal_esp/Defines.h
@@ -1,16 +1,16 @@
 // MACRO/PIN-OUT DEFINITIONS #########################################################################
 
-// Motor A pins
+// Motor A PINOUT
 #define PWM_A 6
 #define INA_1 8
 #define INA_2 7
 
-// Motor B pins
+// Motor B PINOUT
 #define PWM_B 12
 #define INB_1 10
 #define INB_2 11
 
-// Voltimeter pins
+// Voltimeter PINOUT
 #define V_PIN 4 // ADC1_CHANNEL_5
 //#define V_PIN 34 // ADC1_CHANNEL_6
 #define R1 100000
@@ -18,12 +18,13 @@
 #define V_MIN 9.0
 #define MEASURE_TIME 10000
 
-// Led, Speaker pins
+// Led, Speaker PINOUT
 #define SPEAKER_PIN 36
 #define WORKING_PIN 32
 
-// Tasks  
+// Tasks Macro
 #define Task_Stack_Size 30000
+#define Applications_Core 1 
 
 // GIROSCOPE PINS
 #define SDA 32
@@ -32,15 +33,16 @@
 #define OUTPUT_READABLE_YAWPITCHROLL
 
 
-// GIROSCOPE OFFSETS
+// GIROSCOPE OFFSETS OBTAINED WITH MPU6050_Calibration
 #define XGyOffset 62
-#define YGyOffset 0
-#define ZGyOffset 12
-#define XAccOffset -282
-#define YAccOffset -42
-#define ZAccOffset 1856
+#define YGyOffset -7
+#define ZGyOffset 11
+#define XAccOffset -338
+#define YAccOffset -44
+#define ZAccOffset 1500
 
 // PID CONSTANTS
-#define kP 10
-#define kI 10
-#define kD 10
+#define kP 1.0
+#define kI 0.0
+#define kD 0.0
+
--- src/principal_esp/principal_esp.ino
+++ src/principal_esp/principal_esp.ino
@@ -11,7 +11,7 @@
 #include "Wire.h"
 #endif
 
-#include "Defines.h"
+#include "Defines.h" //include all constant definitions
 
 //#######################################################################################################
 // OBJECTS ##############################################################################################
@@ -27,19 +27,16 @@ MPU6050 giroscope;
 // GLOBAL VALUES ########################################################################################
 //#######################################################################################################
 
-int Motor_Way = 0;
-int PWM;
-int Applications_core  = 1;
-float Read_Voltage;
-float pidGoal;
-float giroYaw;
+int Motor_Way; // global value for the way of a motor, received over BT
+int PWM; // global value for the PWM that will be written to a motor
+float Read_Voltage; // global value for voltimeter measure
 
 //#######################################################################################################
 // GIRO - MPU6050 - DMP ##################################################################################
 //#######################################################################################################
-// giroscope control/status vars
+// MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
-uint8_t giroscopeIntStatus;   // holds actual interrupt status byte from giroscope
+uint8_t giroscopeIntStatus;   // holds actual interrupt status byte from MPU
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
@@ -56,12 +53,15 @@ float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gra
 
 
 // ===               INTERRUPT DETECTION ROUTINE                ===
-volatile bool giroscopeInterrupt = false;     // indicates whether giroscope interrupt pin has gone high
+volatile bool giroscopeInterrupt = false; // indicates whether giroscope interrupt pin has gone high
+
+// callback for interrupt call from DMP
 void dmpDataReady() {
   giroscopeInterrupt = true;
 }
 
-// ===               GIRO SETUP DETECTION ROUTINE                ===
+// ===                    GIRO SETUP ROUTINE                    ===
+// Setup function that configures MPU6050 for use DMP to get yaw, pitch, row
 void giroSetup() {
   Wire.begin(SDA, SCL);
   Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
@@ -75,7 +75,7 @@ void giroSetup() {
   giroscope.setXGyroOffset(XGyOffset);
   giroscope.setYGyroOffset(YGyOffset);
   giroscope.setZGyroOffset(ZGyOffset);
-  giroscope.setZAccelOffset(ZAccOffset); // 1688 factory default for my test chip
+  giroscope.setZAccelOffset(ZAccOffset);
 
   giroscope.setDMPEnabled(true);
   attachInterrupt(digitalPinToInterrupt(INTERRUPT), dmpDataReady, RISING);
@@ -84,21 +84,23 @@ void giroSetup() {
 
   // get expected DMP packet size for later comparison
   packetSize = giroscope.dmpGetFIFOPacketSize();
+
 }
 
 //#######################################################################################################
 //FIRMWARE TASKS (THREADS) ##############################################################################
 //#######################################################################################################
+
+// performs all BT connection and data handle for the firmware
 void bluetooth(void * pvParameters) {
   for (;;) {
-      //Serial.println("\t BT");
+
   }
 }
 
+// performs baterry measure and warning low voltage values
 void voltimeter(void * pvParameters) {
   for (;;) {
-    //Serial.println("voltimeter");
-
     Read_Voltage = Voltimeter.getVoltage();
 
     if (Read_Voltage < V_MIN) {
@@ -107,19 +109,18 @@ void voltimeter(void * pvParameters) {
     else {
       digitalWrite(SPEAKER_PIN, LOW);
     }
-
     delay(MEASURE_TIME);
   }
 }
 
+// proccess raw MPU6050 values fusion for yaw, pitch, row values
 void giro(void * pvParameters) {
 
   for (;;) {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
 
-    while (!giroscopeInterrupt && fifoCount < packetSize) {
-
-    }
-
+ 
     // reset interrupt flag and get INT_STATUS byte
     giroscopeInterrupt = false;
     giroscopeIntStatus = giroscope.getIntStatus();
@@ -129,77 +130,79 @@ void giro(void * pvParameters) {
 
     // check for overflow (this should never happen unless our code is too inefficient)
     if ((giroscopeIntStatus & 0x10) || fifoCount == 1024) {
-      // reset so we can continue cleanly
-      giroscope.resetFIFO();
-      //Serial.println(F("FIFO overflow!"));
+        // reset so we can continue cleanly
+        giroscope.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
 
-      // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (giroscopeIntStatus & 0x02) {
-      // wait for correct available data length, should be a VERY short wait
-      while (fifoCount < packetSize) fifoCount = giroscope.getFIFOCount();
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = giroscope.getFIFOCount();
 
-      // read a packet from FIFO
-      giroscope.getFIFOBytes(fifoBuffer, packetSize);
+        // read a packet from FIFO
+        giroscope.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
 
-      // track FIFO count here in case there is > 1 packet available
-      // (this lets us immediately read more without waiting for an interrupt)
-      fifoCount -= packetSize;
-      
-     
         // display Euler angles in degrees
-      giroscope.dmpGetQuaternion(&q, fifoBuffer);
-      giroscope.dmpGetGravity(&gravity, &q);
-      giroscope.dmpGetYawPitchRoll(ypr, &q, &gravity);
-           
+        giroscope.dmpGetQuaternion(&q, fifoBuffer);
+        giroscope.dmpGetGravity(&gravity, &q);
+        giroscope.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        delay(2);
     }
   }
 }
 
 
+// performs PID correction under yaw values and vision received values
 void PID(void * pvParameters) {
-  float yaw;
-  float output;
+  float yaw;              // yaw value for DMP read 
+  float output;           // output value for PID
+  float vision_reference; // angular value received from vision
+
+  pid.setGoal(0.0);
+
   for (;;) {
   
-    yaw = ypr[0] * 180 / M_PI;  
-    //direcao do giro
-    pid.updateReading(yaw);
+    yaw = (ypr[0] * 180 / M_PI) + 180.0;  
+    vision_reference = 100.0;
 
-    //direcao recebida visao
-    pid.setGoal(100.0);
-
+    pid.updateReading(yaw-vision_reference);
     output = pid.control();
 
-    Serial.print("GIRO YAW ");
-    Serial.print(yaw);
-    Serial.print("\tPID ");
-    Serial.println(output);
+    // Serial.print(ypr[0]);
+    // Serial.print("\tGIRO YAW ");
+    // Serial.print(yaw);
+    // Serial.print("\t \t PID ");
+    // Serial.println(output);
 
-    PWM = 255;
-
-    //motorA.enable(PWM, Motor_Way);
-    //motorB.enable(PWM, -1 * Motor_Way);
-    delay(1);
-  
+   // delay(2);  
   }
 }
 
+// enable motors with PID correction values and way values 
 void enableMotors(void * pvParameters) {
   for (;;) {
-//
-//    PWM = 255;
-//
+
 //    motorA.enable(PWM, Motor_Way);
 //    motorB.enable(PWM, -1 * Motor_Way);
+
   }
 }
 
 //#######################################################################################################
 // USEFUL FUNTCIONS  ####################################################################################
 //#######################################################################################################
+
+// setup all pins used for the firmware, INPUT and OUTPUT modes
 void setPinModes(){
     pinMode(WORKING_PIN, OUTPUT);
     pinMode(SPEAKER_PIN, OUTPUT);
+    pinMode(22, OUTPUT);
+    pinMode(INTERRUPT, INPUT);
+
 }
 
 //#######################################################################################################
@@ -207,19 +210,20 @@ void setPinModes(){
 //#######################################################################################################
 void setup() {
   Serial.begin(115200);
-
+  
   giroSetup();
   setPinModes();
+
+  digitalWrite(22, LOW);
   
   digitalWrite(WORKING_PIN, HIGH);
-  //Serial.println("oi");
-  xTaskCreatePinnedToCore(giro, "giro", Task_Stack_Size, NULL, 0, NULL, Applications_core);
-  xTaskCreatePinnedToCore(bluetooth, "bluetooth", Task_Stack_Size, NULL, 0, NULL, Applications_core);
-  xTaskCreatePinnedToCore(voltimeter, "voltimeter", Task_Stack_Size, NULL, 0, NULL, Applications_core);
-  delay(10000);
+  xTaskCreatePinnedToCore(giro, "giro", Task_Stack_Size, NULL, 0, NULL, Applications_Core);
+  xTaskCreatePinnedToCore(bluetooth, "bluetooth", Task_Stack_Size, NULL, 0, NULL, Applications_Core);
+  xTaskCreatePinnedToCore(voltimeter, "voltimeter", Task_Stack_Size, NULL, 0, NULL, Applications_Core);
+  delay(2000);
   Serial.println("DONE GIRO SETUP");
-  xTaskCreatePinnedToCore(PID, "pid", Task_Stack_Size, NULL, 0, NULL, Applications_core);
-  //xTaskCreatePinnedToCore(enableMotors, "enableMotors", Task_Stack_Size, NULL, 0, NULL, Applications_core);
+  xTaskCreatePinnedToCore(PID, "pid", Task_Stack_Size, NULL, 0, NULL, Applications_Core);
+  //xTaskCreatePinnedToCore(enableMotors, "enableMotors", Task_Stack_Size, NULL, 0, NULL, Applications_Core);
 
   digitalWrite(WORKING_PIN, LOW);
 }
