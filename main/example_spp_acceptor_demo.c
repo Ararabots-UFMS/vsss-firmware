2b0b7f8f2f6035d89f76efe7e5f9932ffe8c99f2
Unknown <mauro_bambil@hotmail.com>
2018-09-28 14:37:50 -0400

    definitions.h and parser_params function
    addition of definitions files and parser_params function  in example_spp_acceptor_demo.c

--- main/example_spp_acceptor_demo.c
+++ main/example_spp_acceptor_demo.c
@@ -20,19 +20,19 @@
 #include "esp_gap_bt_api.h"
 #include "esp_bt_device.h"
 #include "esp_spp_api.h"
-
+#include "definitions.h"
 #include "time.h"
 #include "sys/time.h"
 
-#define SPP_TAG "Emayel"
+#define SPP_TAG "Eymael"
 #define SPP_SERVER_NAME "SPP_SERVER"
-#define EXCAMPLE_DEVICE_NAME "Emayel"
-
+#define EXCAMPLE_DEVICE_NAME "Eymael"
 
 static const esp_spp_mode_t esp_spp_mode = ESP_SPP_MODE_CB;
 
 static const esp_spp_sec_t sec_mask = ESP_SPP_SEC_AUTHENTICATE;
 static const esp_spp_role_t role_slave = ESP_SPP_ROLE_SLAVE;
+static void parser_params(uint8_t param);
 
 static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
 {
@@ -61,6 +61,7 @@ static void esp_spp_cb(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
     case ESP_SPP_DATA_IND_EVT:
         ESP_LOGI(SPP_TAG, "ESP_SPP_DATA_IND_EVT len=%d handle=%u", param->data_ind.len, *(param->data_ind.data));
         esp_log_buffer_char("Message: ", param->data_ind.data,param->data_ind.len);
+        parser_params(param->data_ind->data);
         break;
     case ESP_SPP_CONG_EVT:
         ESP_LOGI(SPP_TAG, "ESP_SPP_CONG_EVT");
@@ -99,6 +100,43 @@ void esp_bt_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
     }
     }
     return;
+}
+
+static void parser_params(uint8_t param){
+
+switch (param) {
+  case < 4:
+  // implementar movimentação do motor
+    if(param == WALK_FRONT){
+      // chamada dos motores para frente com univector ou sem
+    }
+    else if(param == TURN_RIGHT){
+    // chamada dos motores para a direita com univector ou sem
+    }
+    else if(param == TURN_LEFT){
+    // chamada dos motores para a esquerda com univector ou sem
+    }
+    else if(param == MOON_WALK){
+    // chamada dos motores para tras com univector ou sem
+
+    }
+
+  break;
+  case 4:
+  // implementar robo autonomo sentido horario
+  break;
+  case 7:
+  // implementar robo autonomo sentido anti-horario
+  break;
+  case > 7 && < 15:
+  // implementar update de PID
+  break;
+  default:
+  // tratamento de exceção
+  break;
+}
+
+
 }
 
 void app_main()
@@ -161,4 +199,3 @@ void app_main()
     esp_bt_pin_code_t pin_code;
     esp_bt_gap_set_pin(pin_type, 0, pin_code);
 }
-
