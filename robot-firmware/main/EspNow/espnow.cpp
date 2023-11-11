9d22c11031f76ff6fd1c34db87777ed12a314b5f
Menchik <allan.m@ufms.br>
2023-11-11 14:19:11 -0400

    fix channel selection

--- robot-firmware/main/EspNow/espnow.cpp
+++ robot-firmware/main/EspNow/espnow.cpp
@@ -50,6 +50,7 @@ static void example_wifi_init(void)
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE) );
     ESP_ERROR_CHECK( esp_wifi_start());
+    ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
     ESP_ERROR_CHECK( esp_wifi_internal_set_fix_rate(ESPNOW_WIFI_IF, 1, WIFI_PHY_RATE_MCS6_LGI));
 
 #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
